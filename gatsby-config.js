c74ef90d2741c8522e90312ba345e68329eb741d
Magnea Rún Vignisdóttir <magnea@oz.com>
2018-12-11 11:29:33 +0000

    remove manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,18 +61,6 @@ module.exports = {
     },
 
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/assets/images/favicon.png',
-      },
-    },
-    {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
